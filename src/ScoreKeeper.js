8e2084b4f04403f04411ed311274783aa6e8533f
Steven Winter <stevenwinter.me@gmail.com>
2019-05-29 20:42:34 -0500

    Intial commit

---
+++ src/ScoreKeeper.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class ScoreKeeper extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { score: 0 };
+        this.singleKill = this.singleKill.bind(this);
+        this.tripleKill = this.tripleKill.bind(this);
+    }
+    singleKill(){
+        this.setState({score: this.state.score + 1});
+    }
+    tripleKill() {
+        this.setState(st => {
+            return { score: st.score + 1 };
+        });
+        this.setState(st => {
+            return { score: st.score + 1 };
+        });
+        this.setState(st => {
+            return { score: st.score + 1 };
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Score is: {this.state.score}</h1>
+                <button onClick={this.singleKill}>Single Kill!</button>
+                <button onClick={this.tripleKill}>Triple Kill!</button>
+            </div>
+        );
+    }
+}
+
+export default ScoreKeeper;
